f9fbe95a47ecb306d9e38bbfbd2d9c728d3098e3
Aghabayli Elnur Sahil <Agha_vz85@ITSTEP.LOCAL>
2018-11-12 21:44:17 +0400

    .

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+var name = ""; 
+var emp ;
+var undef=undefined;
+var nll = null;
+
+
+var num = 213.21;
+var str ="String";
+var func = function(){
+
+}
+
+
+console.log(emp);
+
+console.log(str);
+
+
+function func2 () {
+   
+}
+
+
+
+var x = 5;
+var y = 10;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+
+
+
+
+console.log(func2.toString());
+
+
+var bool = true;
