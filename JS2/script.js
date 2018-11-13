0ea2724df8acf6eb82dadfaf06ec5a86cb56966d
Aghabayli Elnur Sahil <Agha_vz85@ITSTEP.LOCAL>
2018-11-13 21:39:00 +0400

    x

---
+++ JS2/script.js
@@ -0,0 +1,34 @@
+// var a;
+// while(true)
+// {
+//     a = prompt("What is your name?");
+//     if(a)
+//     break; 
+// }   
+
+// console.log(a);
+
+
+
+
+
+
+
+
+
+var a = 0;
+    a = prompt("Enter num ")
+    while(true)
+    {
+    if(a > -100 && a < 100 )
+    {
+        console.log("True");
+        break;
+    }
+    else
+    {
+        console.log("False");
+        break;
+    }
+}
+
