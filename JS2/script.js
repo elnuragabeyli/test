b649e20ac4aff726f93a58d95fbfb000de892baf
Aghabayli Elnur Sahil <Agha_vz85@ITSTEP.LOCAL>
2018-11-14 21:43:11 +0400

    .

--- JS2/script.js
+++ JS2/script.js
@@ -8,27 +8,61 @@
 
 // console.log(a);
 
+ 
+
+    // var string = prompt("Enter name: ")
+    // var a="";
+    
+    // for(var i = 1; i < string.length;i++ )
+    // {
+    //     a+= string[i];
+    // }
+    // for(var i = string.length-1;i>=0 ;i-- )
+    // {
+    //   a += string[i];
+    // }
+    
+    //var a =  string.split("").reverse().join("");
+
+ 
+//    var a = string.substr(1);
+
+//    console.log(parseInt(a));
+ 
+ //---------------------------------------------------------
+    // function trancate(str,num){
+    //     return str.slice(0,num); 
+    // }
+
+    // var str = prompt("Enter word: ");
+    // var num = parseInt( prompt("Enter number: "));
+
+    // console.log(trancate(str,num));
 
 
-
-
-
-
-
-
-var a = 0;
-    a = prompt("Enter num ")
-    while(true)
-    {
-    if(a > -100 && a < 100 )
-    {
-        console.log("True");
-        break;
+    
+    function checkSpam(str){
+        var a = str.toUpperCase();
+        if(a.search("DIROL",0) >= 0 ){
+             console.log("true"); 
+        }
+        else if(a.search("TWIX",0) >= 0 ){
+            console.log("true");
+        } 
+        else if(a.search("SNICKERS",0) >= 0 ){ 
+            console.log("true"); 
+        }
+        else{
+             console.log("False")
+        }
     }
-    else
-    {
-        console.log("False");
-        break;
-    }
-}
+
+    var str = prompt("Enter word: ");
+     
+    console.log(checkSpam(str));
+    
+
+
+       
+ 
 
