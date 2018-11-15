4cd434833fd9df15cde18cab9a89584414a629ba
Aghabayli Elnur Sahil <Agha_vz85@ITSTEP.LOCAL>
2018-11-15 21:47:44 +0400

    .

--- JS2/script.js
+++ JS2/script.js
@@ -41,28 +41,80 @@
 
 
     
-    function checkSpam(str){
-        var a = str.toUpperCase();
-        if(a.search("DIROL",0) >= 0 ){
-             console.log("true"); 
-        }
-        else if(a.search("TWIX",0) >= 0 ){
-            console.log("true");
-        } 
-        else if(a.search("SNICKERS",0) >= 0 ){ 
-            console.log("true"); 
-        }
-        else{
-             console.log("False")
-        }
-    }
+    // function checkSpam(str){
+    //     var a = str.toUpperCase();
+    //     if(a.search("DIROL",0) >= 0 ){
+    //          console.log("true"); 
+    //     }
+    //     else if(a.search("TWIX",0) >= 0 ){
+    //         console.log("true");
+    //     } 
+    //     else if(a.search("SNICKERS",0) >= 0 ){ 
+    //         console.log("true"); 
+    //     }
+    //     else{
+    //          console.log("False")
+    //     }
+    // }
 
-    var str = prompt("Enter word: ");
-     
-    console.log(checkSpam(str));
+    // var str = prompt("Enter word: ");
     
+    // console.log(checkSpam(str));
 
 
-       
+
+
+    // function Join(arr,str)
+    // { 
+    //     var string = "";
+    //     for(var i = 0; i < arr.length;i++ )
+    //     {
+    //         string += arr[i] + str;
+    //     }
+    //    var a = string.slice(0, 0 - str.length);
+    //      return a;
+    // }
+
+    // console.log(Join([1,2,3,4,5], "asdasdasds"));
+    
+   
+
+    // function split(strArr,splitter)
+    // {
+    //     var arr = [];
+    //     var match = 0;
+    //     while(strArr.indexOf(splitter,match) !== -1 )
+    //     { 
+    //          var matchedIndex = strArr.indexOf(splitter,match);
+    //          console.log(matchedIndex);
+    //          arr.push(strArr.slice(match,matchedIndex));
+    //          match = ++matchedIndex;
+    //     } 
+    //     arr.push(strArr.slice(matchedIndex,strArr.length));
+    //     return arr; 
+    // }
+
+    // console.log(split("elnur , alim , hesen",","));
+
+
+
+
+function deleteOnIndex(arr,index)
+{ 
+    var result = [];
+    if(index < 0)
+    {
+        throw new error("This is an exception");
+    }
+    else if(index > arr.length)
+    {
+        for(var i = 0; i < arr.length;i++)
+        {
+           
+        }
+      
+    }
  
+}
 
+     console.log(deleteOnIndex("1,2,3,4","10"));
